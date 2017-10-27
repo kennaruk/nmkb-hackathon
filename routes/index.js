08dd72d54823dc0a4df53537f3bafbc79932b5d2
Ken Nitipat <k-e-n_ken@hotmail.com>
2017-10-28 01:52:36 +0700

    C R firebase added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../db.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // db.writeUserData(1, "name", "email", "image");
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next) {
+
+});
+
 
 module.exports = router;
