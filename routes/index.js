2d89408adefc1fcce505db0b7ed13fec194ee0c4
Ken Nitipat <k-e-n_ken@hotmail.com>
2017-10-28 01:56:10 +0700

    merge with index.ejs

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db.js');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // db.writeUserData(1, "name", "email", "image");
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/', function(req, res, next) {
-
+router.get('/homepage', function(req, res, next) {
+  res.render('index.ejs');
 });
 
 
